c6e0db01cb89d90821619c0190d67852c84b73bb
GuillermoPoblete <guillermo.facundo.poblete@gmail.com>
2024-03-23 09:42:28 -0300

    Update 3_Langchain_Quickstart.py

--- pages/3_Langchain_Quickstart.py
+++ pages/3_Langchain_Quickstart.py
@@ -17,6 +17,6 @@ with st.form("my_form"):
     text = st.text_area("Enter text:", "What are 3 key advice for learning how to code?")
     submitted = st.form_submit_button("Submit")
     if not openai_api_key:
-        st.info("Please add your GuilleFP1 OpenAI API key to continue.")
+        st.info("Please add your GuilleFP1! OpenAI API key to continue.")
     elif submitted:
         generate_response(text)
